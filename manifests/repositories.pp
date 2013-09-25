22d021778ffa6160e56095996ae5b0217cfea832
Luke Kreczko <lkreczko@gmail.com>
2013-09-25 16:44:39 +0100

    replacing emirepos::emi2repositories with full repo definitions for now (emirepos not available outside cernops)

--- manifests/repositories.pp
+++ manifests/repositories.pp
@@ -1,20 +1,50 @@
 class glexecwn::repositories {
-  
   #
-  # ensure that we get everything from EMI and not mix with EPEL unless we need stuff from there
+  # ensure that we get everything from EMI and not mix with EPEL unless we need
+  # stuff from there
   #
-  
-  include 'emirepos::emi2repositories'
-  
+
+#  include 'emirepos::emi2repositories'
+
+  yumrepo { 'EMI_3_base':
+    baseurl  => "http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/\$basearch/base",
+    gpgcheck => 1,
+    gpgkey   => 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi',
+    enabled  => 1,
+    priority => 40,
+  }
+
+  yumrepo { 'EMI_3_contribs':
+    baseurl  => "http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/\$basearch/contribs",
+    gpgcheck => 1,
+    gpgkey   => 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi',
+    enabled  => 1,
+    priority => 40,
+  }
+
+  yumrepo { 'EMI_3_third-party':
+    baseurl  => "http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/\$basearch/third-party",
+    gpgcheck => 1,
+    gpgkey   => 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi',
+    enabled  => 1,
+    priority => 40,
+  }
+
+  yumrepo { 'EMI_3_updates':
+    baseurl  => "http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/\$basearch/updates",
+    gpgcheck => 1,
+    gpgkey   => 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi',
+    enabled  => 1,
+    priority => 40,
+  }
+
   #
   # upgrade path conflicts
   #
 
   # conflict with voms-client3
-  #package {voms-clients:
+  # package {voms-clients:
   #  ensure => absent,
   #}
-
 }
-  
 
