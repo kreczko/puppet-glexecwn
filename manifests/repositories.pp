5a126af50ee497d76cd6615d3737d6ed45009e5d
Luke Kreczko <lkreczko@gmail.com>
2013-09-26 15:52:33 +0100

    fixing warning regarding empty repo name

--- manifests/repositories.pp
+++ manifests/repositories.pp
@@ -7,6 +7,7 @@ class glexecwn::repositories {
   #  include 'emirepos::emi2repositories'
 
   yumrepo { 'EMI_3_base':
+    name => 'EMI 3 base repository',
     baseurl  => "http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/\$basearch/base",
     gpgcheck => 1,
     gpgkey   => 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi',
@@ -15,6 +16,7 @@ class glexecwn::repositories {
   }
 
   yumrepo { 'EMI_3_contribs':
+    name => 'EMI 3 constribs repository',
     baseurl  => "http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/\$basearch/contribs",
     gpgcheck => 1,
     gpgkey   => 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi',
@@ -23,6 +25,7 @@ class glexecwn::repositories {
   }
 
   yumrepo { 'EMI_3_third-party':
+    name => 'EMI 3 third-party repository',
     baseurl  => "http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/\$basearch/third-party",
     gpgcheck => 1,
     gpgkey   => 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi',
@@ -31,6 +34,7 @@ class glexecwn::repositories {
   }
 
   yumrepo { 'EMI_3_updates':
+    name => 'EMI 3 updates repository',
     baseurl  => "http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/\$basearch/updates",
     gpgcheck => 1,
     gpgkey   => 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi',
