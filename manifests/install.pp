a4ebe05e0d45f3ba8a55ce9d4712e3768553ee48
Luke Kreczko <lkreczko@gmail.com>
2013-09-27 15:46:30 +0100

    fixing Puppet style issues ("->', http://docs.puppetlabs.com/guides/style_guide.html#quoting and class name). '-' in class name not generally supported on puppet clients

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,16 +8,16 @@ class glexecwn::install {
   }
   
   # install worker node software
-  class {"emi_wn":}
-  class {"emi_glexec_wn":}
+  class {'emi_wn':}
+  class {'emi_glexec_wn':}
   
-  exec {"glexecperms":
+  exec {'glexecperms':
     command => '/bin/chgrp glexec /usr/sbin/glexec ; /bin/chmod 06111 /usr/sbin/glexec'
   }
 
   # setup environment for glExec WN
   include ('glexecwn::env')
-  include ('glexecwn::site-env')
+  include ('glexecwn::site_env')
 
   # configure VOs
   class {'vosupport':
@@ -25,12 +25,12 @@ class glexecwn::install {
     enable_mappings_for_service => 'ARGUS'
   }
 
-  file {"/var/log/glexec":
-    ensure => "directory",
-    owner => "root",
-    group => "root",
+  file {'/var/log/glexec':
+    ensure => 'directory',
+    owner => 'root',
+    group => 'root',
   }
   
-#  Class["glexecwn::repositories"]->Package["dummydpm","emi-wn","emi-glexec_wn"] -> Exec["glexecperms"] -> File["/var/log/glexec"] -> Class["vosupport","glexecwn::env","glexecwn::site-env"]
-  Class["glexecwn::repositories"] -> Package["dummydpm"] -> Class["emi_wn","emi_glexec_wn"] -> Exec["glexecperms"] -> File["/var/log/glexec"] -> Class["vosupport","glexecwn::env","glexecwn::site-env"]
+#  Class['glexecwn::repositories']->Package['dummydpm','emi-wn','emi-glexec_wn'] -> Exec['glexecperms'] -> File['/var/log/glexec'] -> Class['vosupport','glexecwn::env','glexecwn::site-env']
+  Class['glexecwn::repositories'] -> Package['dummydpm'] -> Class['emi_wn','emi_glexec_wn'] -> Exec['glexecperms'] -> File['/var/log/glexec'] -> Class['vosupport','glexecwn::env','glexecwn::site_env']
 }
