e9a0e4ed5cf24c47e16828ff78e115f9dd5a2b97
Luke Kreczko <lkreczko@gmail.com>
2013-10-01 14:05:39 +0100

    These are several commits in one: 1) Add parameters to the necessary modules to make clear what is used in the sames.    Now the module can be used with Foreman and scripts (as well as hiera) 2) Changing the structure slightly:     a) removing site-env.pp (merged with env.pp) as they have same parameters     b) adding config.pp and moving configuration (including dependencies) in there 3) Adding 'Scientific' to supported OS (the fact defaults to that on SL 6.4) 4) making dummydpm and vosupport (see cernops/puppet-vosupport/#1) optional 5) changing exec to change file permissions to file class (otherwise it runs every time!)

--- manifests/env.pp
+++ manifests/env.pp
@@ -1,27 +1,48 @@
-class glexecwn::env inherits glexecwn::params {
-
-  concat{$gridenvfile:
-    owner =>  'root',
-    group =>  'root',
-    mode  =>  '0755',
-    warn => '# $gridenvfile is managed by Puppet env.pp.\n#Any changes in here will be overwritten',         
+class glexecwn::env (
+  $gridenvfile        = $glexecwn::params::gridenvfile,
+  $srm_path           = $glexecwn::params::srm_path,
+  $site_name          = $glexecwn::params::site_name,
+  $myproxy_server     = $glexecwn::params::myproxy_server,
+  $lcg_gfal_infosys   = $glexecwn::params::lcg_gfal_infosys,
+  $lcg_location       = $glexecwn::params::lcg_location,
+  $gt_proxy_mode      = $glexecwn::params::gt_proxy_mode,
+  $grid_env_location  = $glexecwn::params::grid_env_location,
+  $gridmapdir         = $glexecwn::params::gridmapdir,
+  $glite_location     = $glexecwn::params::glite_location,
+  $glite_location_var = $glexecwn::params::glite_location_var,
+  $glite_env_set      = $glexecwn::params::glite_env_set,
+  $glexec_location    = $glexecwn::params::glexec_location,) {
+  concat { $gridenvfile:
+    owner => 'root',
+    group => 'root',
+    mode  => '0755',
+    warn  => '# $gridenvfile is managed by Puppet env.pp.\n#Any changes in here will be overwritten',
   }
-  concat::fragment{'grid-env header': 
+
+  concat::fragment { 'grid-env header':
     target  => $gridenvfile,
     order   => '01',
     content => template('glexecwn/gridenvsh_header.erb')
   }
-  concat::fragment{'grid-env footer': 
+
+  concat::fragment { 'grid-env footer':
     target  => $gridenvfile,
     order   => '99',
     content => template('glexecwn/gridenvsh_footer.erb')
   }
-  
-  file {'/etc/profile.d/grid-env.csh':
-    ensure => present,
+
+  file { '/etc/profile.d/grid-env.csh':
+    ensure  => present,
     content => template('glexecwn/gridenvcsh.erb'),
-    owner => 'root',
-    group => 'root',
-    mode => 0644,
-  }  
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+  }
+
+  # site-env
+  concat::fragment { 'site-env global':
+    target  => $gridenvfile,
+    order   => '55',
+    content => template('glexecwn/gridenv-site.sh.erb')
+  }
 }
