ba9f5fa68fcaabc570eb502cc0b4fb54dc216476
fschaer <frederic.schaer@cea.fr>
2015-02-25 14:37:16 +0100

    allow for specifying the lcmaps/glexec templates, so that glexec can be used with condor's "mount under scratch" feature without breaking lcmaps

--- manifests/argus.pp
+++ manifests/argus.pp
@@ -1,10 +1,17 @@
 class glexecwn::argus (
   $argus_server    = $glexecwn::params::argus_server,
   $argus_port      = $glexecwn::params::argus_port,
-  $user_white_list = $glexecwn::params::user_white_list) {
+  $user_white_list = $glexecwn::params::user_white_list,
+  $template_glexec_conf = 'glexecwn/glexec.conf.erb',
+  $template_lcmaps_db   = 'glexecwn/lcmaps-glexec.db.erb',
+  $loglevel_glexec = 3,
+  $loglevel_lcmaps = 1,
+  $loglevel_lcas   = 1,
+  $loglevel_debug  = 1, #this is for various debuf log levels in the config
+  ) {
   file { '/etc/glexec.conf':
     ensure  => present,
-    content => template('glexecwn/glexec.conf.erb'),
+    content => template($template_glexec_conf),
     owner   => 'root',
     group   => 'glexec',
     mode    => 0640,
@@ -12,7 +19,7 @@ class glexecwn::argus (
 
   file { '/etc/lcmaps/lcmaps-glexec.db':
     ensure  => present,
-    content => template('glexecwn/lcmaps-glexec.db.erb'),
+    content => template($template_lcmaps_db),
     owner   => 'root',
     group   => 'root',
     mode    => 0640,
